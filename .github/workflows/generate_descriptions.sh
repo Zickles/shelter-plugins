c6374831c9b11c435f02a1ef087b4690de00b603
Zickles <76439587+Zickles@users.noreply.github.com>
2024-03-30 11:05:02 -0400

    fix link

--- .github/workflows/generate_descriptions.sh
+++ .github/workflows/generate_descriptions.sh
@@ -4,7 +4,7 @@ echo "shelter.stay-mad.lol" >> dist/CNAME
 for plugin in $(cd plugins/; ls); do
   echo "## $(jq -r .name < "plugins/$plugin/plugin.json")">>dist/README.md
   echo "" >> dist/README.md
-  echo "Install url: \`https://shelter.stay-mad.lol/shelter-plugins/$plugin\`">> dist/README.md
+  echo "Install url: \`https://shelter.stay-mad.lol/$plugin\`">> dist/README.md
   echo "" >> dist/README.md
   echo "$(jq -r .description < "plugins/$plugin/plugin.json")" >> dist/README.md
   echo "" >> dist/README.md
@@ -13,7 +13,7 @@ for plugin in $(cd plugins/; ls); do
   echo "# $(jq -r .name < "plugins/$plugin/plugin.json")">>dist/"$plugin"/README.md
   echo "> **Note:** You are on a plugin page, probably by clicking one of the plugin links. You need to copy the link address and paste it into the shelter settings." >>dist/"$plugin"/README.md
   echo "" >>dist/"$plugin"/README.md
-  echo "Install url: \`https://shelter.stay-mad.lol/shelter-plugins/$plugin\`">>dist/"$plugin"/README.md
+  echo "Install url: \`https://shelter.stay-mad.lol/$plugin\`">>dist/"$plugin"/README.md
   echo "" >>dist/"$plugin"/README.md
   echo "$(jq -r .description < "plugins/$plugin/plugin.json")" >>dist/"$plugin"/README.md
   echo "" >>dist/"$plugin"/README.md
